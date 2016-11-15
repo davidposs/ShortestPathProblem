546708dbd0344af031a22370ef7353e89478b35c
davidposs <davidp525@gmail.com>
2016-11-15 00:03:49 -0800

    First commmit

---
+++ inna.cpp
@@ -0,0 +1,137 @@
+//*****************************************************************************
+// David Poss
+// CPSC 335
+// Project 3
+// 10/26/2016
+//*****************************************************************************
+
+#include <iostream>
+#include <chrono>
+#include <math.h>
+
+struct point {
+    float x, y; //x and y coordinates of graph
+};
+
+void print_cycle(int, point*, int*);
+int farthest_point(int, point*);//returns the index the furthest point
+int nearest(int, point*, int, bool*);//finds the nearest unvisited neighbor
+
+int main() {
+
+    point *G; //pointer to points, a Graph
+    int *M;   //pointer to vertices in G that form estimated shrotest path
+    bool *Visited; //whether or not a point in G has been visited
+    int i, n;
+    float dist;
+    int A, B;
+    std::cout << "Enter the number of vertices (>2): "; 
+    std::cin >> n;
+    if (n < 3) {
+        return 0;
+    }
+    G = new point[n];
+
+    std::cout << "Enter the points; make sure they are distinct" << std::endl;
+    for(i = 0; i < n; i++) {
+        std::cout << "  Point " << i << ": (x,y) = ";
+        std::cin >> G[i].x >> G[i].y;
+    }
+    M = new int[n]; //M holds the order of indicesi of G for the best cycle
+    for(i = 0; i < n; i++) {
+        M[i]=i;
+    }
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    //To track if a point has been visited, so we don't add it twice
+    Visited = new bool[n];
+    for(i = 0; i < n; i++) {
+        Visited[i] = false;
+    }
+
+    //Calculator the two furthest points, and choose 1 to start from
+    A = farthest_point(n,G);
+    M[0]= A;
+    Visited[A] = true;
+
+    for(i = 1; i < n; i++) {
+        B = nearest(n, G, A, Visited); //B = nearest point to A
+        //A = B; //set 
+        M[i] = B; //add B to the list
+        A = B;
+        Visited[A] = true;
+    }
+
+
+    dist = 0; //Calculate the distance for our Hamiltonian cycle
+    for (i = 0; i < n - 1; i++) {
+        dist += sqrt((G[M[i]].x - G[M[i+1]].x)*(G[M[i]].x - G[M[i+1]].x) +
+                (G[M[i]].y - G[M[i+1]].y)*(G[M[i]].y - G[M[i+1]].y));
+    }
+    dist += sqrt((G[M[0]].x - G[M[n-1]].x)*(G[M[0]].x - G[M[n-1]].x) +
+            (G[M[0]].y - G[M[n-1]].y)*(G[M[0]].y - G[M[n-1]].y));
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << "The Hamiltonian cycle of a relative minimum length " << std::endl;
+    print_cycle(n, G, M);
+    std::cout << "The relative minimum length is " << dist << std::endl;
+
+    int microseconds = 
+        std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    double seconds = microseconds / 1E6;
+    std::cout << "elapsed time: " << seconds << " seconds" << std::endl;
+
+    delete [] G;
+    delete [] M;
+    return 0;
+}
+
+//calculates two farthest points, returns 1 of them
+int farthest_point(int n, point *P) {
+    float max_dist = 0;
+    float dist;
+    int bestPoint = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            dist = pow(P[i].x - P[j].x, 2) + pow(P[i].y - P[j].y, 2);
+            if (max_dist < dist) {
+                max_dist = dist;
+                bestPoint = i;
+            }
+        }
+    }
+    return bestPoint;
+}
+    
+int nearest(int n, point *P, int A, bool *Visited) {
+//finds nearest point to P[A];
+    int curr_dist;
+    int nearest_dist = 0;
+    int closest_point = A;
+    bool Nearest_DistAssigned = false;
+    for (int i = 0; i < n; i++) {
+        if (i == A)
+            continue;
+        if (!Visited[i]) {
+            curr_dist = sqrt(pow(P[A].x - P[i].x, 2) + pow(P[A].y - P[i].y, 2));
+            if (!Nearest_DistAssigned) {
+                nearest_dist = curr_dist;
+                closest_point = i;
+                Nearest_DistAssigned = true;
+            }
+            if (nearest_dist > curr_dist) {
+                nearest_dist = curr_dist;
+                closest_point = i;
+            }
+        }
+    }
+    return closest_point;
+}
+void print_cycle(int n, point* G, int* seq) {
+    for (int i = 0; i < n; i++) {
+        std::cout << "(" << G[seq[i]].x << "," << G[seq[i]].y << "), ";
+    }
+    std::cout << "(" << G[seq[0]].x << "," << G[seq[0]].y << ")";
+    std::cout << std::endl;
+}
---
+++ proj3.cpp
@@ -0,0 +1,125 @@
+//*****************************************************************************
+// David Poss
+// CPSC 335
+// Project 3
+// 10/26/2016
+//*****************************************************************************
+
+#include <iostream>
+#include <chrono>
+#include <math.h>
+
+struct point {
+    float x, y; //x and y coordinates of graph
+};
+
+float farthest(int n, point* graph);
+void print_cycle(int n, point* G, int* seq);
+void print_perm(int n, int *A, int sizeA, point* G, 
+        int* bestSet, float& bestDist);
+
+int main() {
+    int n; //number of vertices in the graph
+    std::cout << "Enter the number of vertices (>2): ";
+    std::cin >> n;
+    if (n < 3) {
+        std::cout << "n must be higher than 2\n";
+        return 0;
+    }
+    point* graph = new point[n]; //graph is a collection of points
+
+    std::cout << "Enter the points, making sure they are distinct: \n";
+    for (int i = 0; i < n; i++) {
+        std::cout << "  Point " << i << ": (x,y) = ";
+        std::cin >> graph[i].x >> graph[i].y;
+    }
+
+    int* bestSet = new int[n + 1]; //n + 1 since we need to go back to start
+    auto start = std::chrono::high_resolution_clock::now();
+    float dist = farthest(n, graph);
+    float bestDist = n*dist;
+
+    int* A = new int[n + 1];
+    for (int i = 0; i < n + 1; i++) {
+        A[i] = i;
+        bestSet[i] = 0;
+    }
+
+    print_perm(n, A, n + 1, graph, bestSet, bestDist);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    std::cout << "The Hamiltonian Cycle of minimum length is " << std::endl;
+    print_cycle(n, graph, bestSet);
+    std::cout << "Minimum length is: " << bestDist << std::endl;
+    int microseconds = int(std::chrono::duration_cast<
+            std::chrono::microseconds>(end - start).count());
+    double seconds = microseconds / 1E6;
+    std::cout << "Elapsed time: " << seconds << " seconds" << std::endl;
+    delete[] graph;
+    delete[] A;
+    delete[] bestSet;
+
+    return 0;
+}
+
+void print_perm(int n, int* A, int sizeA, point* G, int* bestSet, float& bestDist) {
+    int i;
+    float dist;
+    if (n == 1) {
+        dist = 0;
+        for (int j = 0; j < sizeA - 2; j++) {
+            dist += sqrt((pow(G[A[j]].x - G[A[j + 1]].x, 2)) 
+                    + pow(G[A[j]].y - G[A[j + 1]].y, 2));
+        }
+        dist += sqrt((pow(G[A[sizeA - 2]].x - G[A[0]].x, 2)) 
+                + pow(G[A[sizeA - 2]].y - G[A[0]].y, 2));
+        if (dist < bestDist) {
+            bestDist = dist;
+            for (int k = 0; k < sizeA; k++) {
+                bestSet[k] = A[k];
+            }
+        }
+    }//end if
+    else {
+        for (i = 0; i < n - 1; i++) {
+            print_perm(n - 1, A, sizeA, G, bestSet, bestDist);
+            if (n % 2 == 0) {
+                int temp = A[i];
+                A[i] = A[n - 1];
+                A[n - 1] = temp;
+            }
+            else {
+                int temp = A[0];
+                A[0] = A[n - 1];
+                A[n - 1] = temp;
+            }
+        }
+        print_perm(n - 1, A, sizeA, G, bestSet, bestDist);
+    }//end else
+}//end of print_perm
+
+float farthest(int n, point* P) {
+    float max_dist = 0;
+    float dist;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n; j++) {
+            dist = (P[i].x - P[j].x)*(P[i].x - P[j].x) 
+                + (P[i].y - P[j].y)*(P[i].y - P[j].y);
+            if (max_dist < dist)
+                max_dist = dist;
+        }
+    return sqrt(max_dist);
+}//end of farthest
+
+void print_cycle(int n, point* G, int* seq) {
+    for (int i = 0; i < n; i++) {
+        std::cout << "(" << G[seq[i]].x << "," << G[seq[i]].y << ")";
+        if (i < n - 1) {
+            std::cout << ", ";
+        }
+        else {  //Reprint first node
+            std::cout << ", (" << G[seq[0]].x << ", " << G[seq[0]].y << ")";
+        }
+    }
+    std::cout << std::endl;
+}//end of print_cycle
